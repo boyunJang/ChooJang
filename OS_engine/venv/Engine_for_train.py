7c188c7d75fcba174ebb69739d1d15e1cdfb4639
hoonihu1 <hoonihu1@gmail.com>
2020-09-01 21:12:46 +0900

    200901_version==Fixed Engine && Server

--- OS_engine/venv/Engine_for_train.py
+++ OS_engine/venv/Engine_for_train.py
@@ -1,60 +1,86 @@
 import tensorflow as tf
+tf.compat.v1.enable_eager_execution()
+import timeit
+import numpy as np
 
-config = {
-    'batch': 50,
-    'train_epch': 10,
-    'param_conv': [3, 3, 3],
-    'conv_active': 'relu',
-    'pooling': 'Max',
-    'param_dense': [(10, 'relu'), (10, 'softmax')],
-    'loss': tf.keras.losses.mean_squared_error
-}
+# config = {
+# 'batch': 50,
+# 'train_epch': 10,
+# 'param_conv': [3, 3, 3],
+# 'conv_active': 'relu',
+# 'pooling': 'Max',
+# 'param_dense': [(10, 'relu'), (10, 'softmax')],
+# 'loss': tf.keras.losses.mean_squared_error
+# }
+
 
 class cg():
-    def __init__(self):
-        self.config = config
-        Model = tf.keras.Sequential()
-        Model.add(tf.keras.layers.Input(shape=(28, 28, 1), batch_size=config['batch']))
-        # conv
-        for param in config['param_conv']:
-            Model.add(tf.keras.layers.Conv2D(10, param, activation=config['conv_active']))
-            if config['pooling'] == 'Max':
-                Model.add(tf.keras.layers.MaxPooling2D())
-            else:
-                Model.add(tf.keras.layers.AveragePooling2D())
+    def __init__(self, param):
+        self.config = param
+        (self.x_train, self.y_train), (self.x_test, self.y_test) = tf.keras.datasets.mnist.load_data()
 
-        Model.add(tf.keras.layers.Flatten())
+        # self.x_train=self.x_train[:50]
+        # self.y_train=self.y_train[:50]
+        # self.x_test=self.x_test[:10]
+        # self.y_test=self.y_test[:10]
+        self.Model = tf.keras.Sequential()
+        self.Model.add(tf.keras.layers.Input(shape=(28, 28, 1), batch_size=self.config['batch']))
+        # conv
+        for param in self.config['param_conv']:
+            self.Model.add(tf.keras.layers.Conv2D(10, param, activation=self.config['conv_active']))
+        if self.config['pooling'] == 'Max':
+            self.Model.add(tf.keras.layers.MaxPooling2D())
+        else:
+            self.Model.add(tf.keras.layers.AveragePooling2D())
+
+        self.Model.add(tf.keras.layers.Flatten())
 
         # dense
-        for (node, activation) in config['param_dense']:
+        for (node, activation) in self.config['param_dense']:
             if activation == 'relu':
-                Model.add(tf.keras.layers.Dense(node, tf.keras.activations.relu))
+                self.Model.add(tf.keras.layers.Dense(node, tf.keras.activations.relu))
             elif activation == 'softmax':
-                Model.add(tf.keras.layers.Dense(node, tf.keras.activations.softmax))
+                self.Model.add(tf.keras.layers.Dense(node, tf.keras.activations.softmax))
 
-        Model.compile(optimizer='adam', loss=tf.keras.losses.sparse_categorical_crossentropy)
-        Model.build()
-        Model.summary()
+        self.Model.compile(optimizer='adam', loss=tf.keras.losses.sparse_categorical_crossentropy)
+        self.Model.build()
+        self.Model.summary()
 
-        # 데이터 구분 필요(mnist 인지 cifa 인지)
-        (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-        x_train, x_test = x_train / 255.0, x_test / 255.0
+    def train(self):
+        x_train, y_train = self.x_train, self.y_train
+        x_train = x_train / 255.0
 
         x_train = x_train[..., tf.newaxis]
+
+        # calculate train time
+        train_start_time = timeit.default_timer()
+        # Model train
+        self.Model.fit(x_train, y_train, epochs=self.config['train_epch'], batch_size=self.config['batch'])
+        # train terminate
+        train_end_time = timeit.default_timer()
+
+        # calculate
+        train_pred = self.Model.predict(x_train)
+        train_pred = tf.math.argmax(train_pred, 1)
+        train_acc = tf.reduce_mean(tf.cast(tf.equal(train_pred, y_train), tf.float32)) * 100
+        train_acc = train_acc.numpy()
+
+        # Sec
+        train_time = train_end_time - train_start_time
+
+        return train_acc, train_time
+
+    def pred(self):
+        x_test = self.x_test
+        x_test = x_test / 255.0
+
         x_test = x_test[..., tf.newaxis]
 
-        train_ds = tf.data.Dataset.from_tensor_slices(
-            (x_train, y_train)).shuffle(10000).batch(32)
-        test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+        pred = self.Model.predict(x_test)
+        pred = tf.math.argmax(pred, 1)
 
-        # Model 학습
-        Model.fit(x_train, y_train, epochs=config['train_epch'], batch_size=config['batch'])
+        acc = tf.reduce_mean(tf.cast(tf.equal(pred, self.y_test), tf.float32)) * 100
+        acc = acc.numpy()
 
-        Model.predict(x_test)
-
-
-if __name__=='__main__':
-    cfg = cg()
-
-
+        return acc
 
--- testWeb/app/main/index.py
+++ testWeb/app/main/index.py
@@ -1,20 +1,21 @@
 # file name : index.py
 # pwd : /project_name/app/main/index.py
 
-from flask import Blueprint, request, render_template, flash, redirect, url_for
+from flask import Blueprint, request, render_template, flash, redirect, url_for, jsonify
 from flask import current_app as app
 from OS_engine.venv import Engine_for_train
+import numpy
 # 추가할 모듈이 있다면 추가
 
 main = Blueprint('main', __name__, url_prefix='/')
 
 testData = {
-    'batch': 50,
-    'train_epch': 10,
-    'param_conv': [3, 3, 3],
+    'batch': 0,
+    'train_epch': 0,
+    'param_conv': [],
     'conv_active': 'relu',
     'pooling': 'Max',
-    'param_dense': [(10, 'relu'), (10, 'softmax')],
+    'param_dense': [],
     'loss': "tf.keras.losses.mean_squared_error"
 }
 
@@ -29,38 +30,31 @@ def index():
 def post():
     # testData['conv_active'] = request.form['conv_active']
     # print(testData)
-    instance = Engine_for_train
-    instance.cg()
+
 
     return render_template('/main/index.html')
 
 @main.route('/test', methods=['POST'])
 def test():
-    req_data = request.get_json()
-    print(req_data)
 
+    req_data = request.get_json(silent=True, force=True)
+    print(testData)
+    print(req_data)
     testData['batch'] = int(req_data['batch'])
     testData['train_epch'] = int(req_data['train_epch'])
     testData['param_conv'] = list(map(int, req_data['param_conv'].split(',')))
     testData['conv_active'] = req_data['conv_active']
     testData['pooling'] = req_data['pooling']
+    for node, act in zip(req_data['param_dense'].split(","), req_data['param_dense_act'].split(",")):
+        testData['param_dense'].append((node, act))
 
+    instance = Engine_for_train.cg(testData)
 
-    # print(list(testData['param_dense'][0]))
-    # print(list(testData['param_dense'][0])[0])
-    # print(type((req_data['param_dense'].split(','))[0]))
-    # print(len(testData['param_dense']))
-    # print(req_data['param_dense'].split(','))
+    print(testData)
+    acc, train_time  = instance.train()
+    test_acc = instance.pred()
+    print('학습률 :', acc, '학습 소요 시간 :', train_time)
+    print('예측률 :', test_acc )
+    print(str(acc)+","+str(train_time)+","+str(test_acc))
 
-    for i in range(len(testData['param_dense'])):
-        # print(list(testData['param_dense'][i])[0])
-        print("req값", req_data['param_dense'].split(',')[i])
-        list(testData['param_dense'][i])[0] = req_data['param_dense'].split(',')[i]
-        print(testData['param_dense'][i][0])
-
-    print(testData['param_dense'])
-    # print(testData)
-
-
-    return req_data
-
+    return str(acc)+","+str(train_time)+","+str(test_acc)
